b16fdd963b94af8ed294db01747c889ffec32fe4
yarqui <y.pelykh@gmail.com>
2025-09-29 13:53:11 -0400

    add force_destroy = true to s3.tf

--- lesson-5/modules/s3-backend/s3.tf
+++ lesson-5/modules/s3-backend/s3.tf
@@ -1,6 +1,8 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.bucket_name
 
+  force_destroy = true
+
   tags = {
     Name        = "Terraform State Bucket"
     ManagedBy   = "Terraform"
