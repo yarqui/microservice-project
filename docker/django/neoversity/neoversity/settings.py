42ada3d84ad75033376217d139adebb240db43cc
yarqui <y.pelykh@gmail.com>
2025-10-08 12:07:24 -0400

    🔧 chore(config): remove temporary comment   - remove comment previously added to trigger argocd deployment
    👷 ci(jenkins): add github project url and push trigger
      - configure github project url for goit-django-docker pipeline
      - enable github push events to trigger goit-django-docker pipeline

--- docker/django/neoversity/neoversity/settings.py
+++ docker/django/neoversity/neoversity/settings.py
@@ -121,5 +121,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-# added this comment to trigger the change for ArgoCD
