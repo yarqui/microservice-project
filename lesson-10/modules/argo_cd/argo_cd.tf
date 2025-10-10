7cb477c9f3d68dc48c8dfada0d9a236e80d97435
yarqui <y.pelykh@gmail.com>
2025-10-10 16:59:56 -0400

    ✨ feat(argo-cd): implement app of apps pattern for application management - switch to kubernetes manifest to define argo cd application resource - introduce app of apps pattern for centralized application management - configure django-app with source, destination, and automated sync policy - pass database credentials for application configuration

--- lesson-10/modules/argo_cd/argo_cd.tf
+++ lesson-10/modules/argo_cd/argo_cd.tf
@@ -18,19 +18,56 @@ resource "helm_release" "argo_apps" {
   namespace  = var.namespace
   create_namespace = false
 
-  values = [
-    templatefile("${path.module}/charts/values.yaml", {
-      github_repo_url = var.github_repo_url
-      github_user     = var.github_user
-      github_pat      = var.github_pat
+  # values = [
+  #   templatefile("${path.module}/charts/values.yaml", {
+  #     github_repo_url = var.github_repo_url
+  #     github_user     = var.github_user
+  #     github_pat      = var.github_pat
 
-      db_host     = var.db_host
-      db_name     = var.db_name
-      db_user     = var.db_user
-      db_password = var.db_password
-    })
-  ]
+  #     db_host     = var.db_host
+  #     db_name     = var.db_name
+  #     db_user     = var.db_user
+  #     db_password = var.db_password
+  #   })
+  # ]
   
   depends_on = [helm_release.argo_cd]
 }
 
+resource "kubernetes_manifest" "argo_app_of_apps" {
+  manifest = templatefile("${path.module}/charts/templates/application.yaml", {
+    # This Values object simulates the Helm .Values object
+    Values = {
+      applications = [
+        {
+          name    = "django-app"
+          project = "default"
+          source = {
+            repoURL        = var.github_repo_url
+            path           = "lesson-10/charts/django-app"
+            targetRevision = "lesson-10"
+            helm = {
+              valueFiles = ["values.yaml"]
+            }
+          }
+          destination = {
+            server    = "https://kubernetes.default.svc"
+            namespace = "production"
+          }
+          syncPolicy = {
+            automated = {
+              prune    = true
+              selfHeal = true
+            }
+          }
+        }
+      ]
+      # These are the values the template needs
+      db_host     = var.db_host
+      db_name     = var.db_name
+      db_user     = var.db_user
+      db_password = var.db_password
+    }
+  })
+  depends_on = [helm_release.argo_cd]
+}
